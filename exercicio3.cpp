76a8f4e1cf14ebb6e349160e41c96a5e10ddd205
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-03-26 23:34:01 +0000

    feito

---
+++ exercicio3.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+
+int main(){
+    float n1,n2,n3;
+    printf("digite o primeiro numero: ");
+    
+    scanf("%f",&n1);
+
+    printf("digite o segundo numero: ");
+    
+    scanf("%f",&n2);
+
+    printf("digite o terceir numero: ");
+  
+    scanf("%f",&n3);
+
+    
+
+    printf("A media dos numeros é: %.2f\n",(n1 + n2 + n3) /3);
+    
+    return false;
+}
