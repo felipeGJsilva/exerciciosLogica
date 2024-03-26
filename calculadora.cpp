c9a617815308b293a5d5254b6f77c5af8ba876b5
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-03-26 22:43:23 +0000

    feito

---
+++ calculadora.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+
+int main(){
+    int n1,n2;
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+    int soma = n1 + n2;
+    printf("A soma dos numeros deu: %d\n", soma);
+}
