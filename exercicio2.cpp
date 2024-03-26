11194c778c1e6579b96ea238e6861afa4e4057aa
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-03-26 22:54:59 +0000

    feito

--- exercicio2.cpp
+++ exercicio2.cpp
@@ -1,5 +1,16 @@
 # include <iostream>
 
 int main(){
+    int n1;
+    printf("Digite o numero: ");
+    scanf("%d", &n1);
     
+    int cal = n1 % 2;
+
+    bool confi = (n1 == 0);
+    
+    printf("Resultado: ", confi);
+
+    return false;
+
 }
