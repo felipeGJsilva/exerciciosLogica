04049978715d2afaa10c3aa2fc54c4dd03354a9e
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-03-30 19:23:13 +0000

    feito

---
+++ exercicio8.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+
+int main(){
+
+    int lado1, lado2, lado3;
+
+    printf("Digite o numero do primeiro lado:  ");
+    scanf("%d", &lado1);
+
+    printf("Digite o numero do segundo lado: ");
+    scanf("%d", &lado2);
+
+    printf("Digite o numero do terceiro lado: ");
+    scanf("%d", &lado3);
+    
+    int soma = lado1 + lado2;
+    printf("%d + %d = %d \n",lado1, lado2,soma );
+
+    bool result = soma > lado3;
+
+    printf("resultado %d", result);
+
+    return false;
+}
