f311468f4f39ecf7ad1b80547e166ff9aa6fb60a
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-02 23:58:19 +0000

    feito

--- exercicio10.cpp
+++ exercicio10.cpp
@@ -1,8 +1,35 @@
 # include <iostream>
 int main(){
 
+    float n1,n2,n3,p1,p2,p3,res;
     
+    printf("digite a nota N ");
 
+    scanf("%f",&n1);
+
+    printf("digite a nota N ");
+
+    scanf("%f",&n2);
+
+    printf("digite a nota N ");
+
+    scanf("%f",&n3);
+
+     printf("digite a nota P ");
+
+    scanf("%f",&p1);
+
+    printf("digite a nota P ");
+
+    scanf("%f",&p2);
+
+    printf("digite a nota P ");
+
+    scanf("%f",&p3);
+
+    res = ((n1 + p1)+ (n2 + p2)+ (n3 + p3)) / (p1 + p2 + p3);
+
+    printf("%f",res);
 
     return false;
 }
