f5bc4e1b0b398b013eafab009e7bafc16108457d
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-03 00:18:24 +0000

    feito

---
+++ exercicio13.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+int main(){
+
+    int h,s ;
+
+    printf("Digite a quantidade de horas: ");
+    scanf("%d",&h);
+    s = h * 3600;
+
+    printf("Em segundos são: %d\n ", s);
+
+    return false;
+}
