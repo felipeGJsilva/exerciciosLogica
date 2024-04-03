634c4a089336071a8c84081007bdcba9fe51b897
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-03 00:06:47 +0000

    feito

---
+++ exercicio11.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+int main(){
+    int b,a;
+
+    printf("Digite a base: ");
+    scanf("%d", &b);
+    printf("Digite a altura: ");
+    scanf("%d", &a);
+    
+    int result = (b*a)/2 ;
+
+    printf("A area do retangulo é %d", result);
+
+}
